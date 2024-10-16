cbba75a3b4a8dc601e2c82f9a84c4f45fddd392b
Hertz <106390207+VictorLemosDEV@users.noreply.github.com>
2024-10-16 12:35:26 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
 app.post('/', (req, res)=>{
