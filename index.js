fa42518cc15c133cb5f88b775d7ee05c913f00be
Hertz <106390207+VictorLemosDEV@users.noreply.github.com>
2024-10-30 16:50:48 -0300

    Fixed Post Route

--- index.js
+++ index.js
@@ -25,23 +25,23 @@ app.use(express.json());
 async function GetDataFromMongoDb(InitializeData) {
 
 
-    
 
 
-        if (InitializeData == false) {
-            var collection = db.collection("App Data");
 
-        let data = await collection.findOne({_id: new ObjectId("66e5d2ebe93fee3b400bf619")})
+    if (InitializeData == false) {
+        var collection = db.collection("App Data");
 
-        return data
-        } else {
-            var collection = db.collection("Arduino Data");
-
-        let data = await collection.findOne({_id: new ObjectId("67227078a64f60cf8cd66109")})
+        let data = await collection.findOne({ _id: new ObjectId("66e5d2ebe93fee3b400bf619") })
 
         return data
-        }
-    
+    } else {
+        var collection = db.collection("Arduino Data");
+
+        let data = await collection.findOne({ _id: new ObjectId("67227078a64f60cf8cd66109") })
+
+        return data
+    }
+
 }
 
 async function PostDataInMongoDb(Data, InitializeData) {
@@ -49,17 +49,17 @@ async function PostDataInMongoDb(Data, InitializeData) {
     if (InitializeData == false) {
         var collection = db.collection("App Data");
 
-        let data = await collection.findOneAndReplace({_id: new ObjectId("66e5d2ebe93fee3b400bf619")}, Data)
+        let data = await collection.findOneAndReplace({ _id: new ObjectId("66e5d2ebe93fee3b400bf619") }, Data)
 
         return data
     } else {
         var collection = db.collection("Arduino Data");
 
-        let data = await collection.findOneAndReplace({_id: new ObjectId("67227078a64f60cf8cd66109")}, Data)
+        let data = await collection.findOneAndReplace({ _id: new ObjectId("67227078a64f60cf8cd66109") }, Data)
 
         return data
     }
-    
+
 }
 
 app.get('/data', async (req, res) => {
@@ -67,7 +67,7 @@ app.get('/data', async (req, res) => {
 
     let data = await GetDataFromMongoDb(false)
 
-    delete(data["_id"])
+    delete (data["_id"])
 
 
 
@@ -83,7 +83,7 @@ app.post('/data', async (req, res) => {
     response = JSON.stringify(response)
 
 
-    
+
 
     res.send(`Publicado no banco de dados:  ${response}`);
 })
@@ -93,8 +93,8 @@ app.get('/initializedata', async (req, res) => {
 
     let data = await GetDataFromMongoDb(true)
 
-    if  (data && data["_id"]) {
-        delete(data["_id"])
+    if (data && data["_id"]) {
+        delete (data["_id"])
     }
 
 
@@ -105,15 +105,14 @@ app.get('/initializedata', async (req, res) => {
 app.post('/initializedata', async (req, res) => {
     // Publicar a varíavel "data" no banco de dados
 
-    const  data  = req.body
+    const data = req.body
     res.send(`O que chegou no servidor: ${data}`)
-    return
-    
+
     if (data) {
         let response = await PostDataInMongoDb(data, true)
-    response = JSON.stringify(response)
+        response = JSON.stringify(response)
 
-    res.send(`Chegou as informações: ${JSON.stringify(response)}`)
+        res.send(`Chegou as informações: ${JSON.stringify(response)}`)
     } else {
         res.status(422).send(`Data parameter not found`)
     }
@@ -131,16 +130,16 @@ async function connectToCluster(uri) {
     let mongoClient
 
     try {
-         mongoClient = await new MongoClient(uri);
+        mongoClient = await new MongoClient(uri);
         console.log('Connecting to MongoDB Atlas cluster...');
         let connection = await mongoClient.connect();
         db = connection.db("TechHome")
         console.log('Successfully connected to MongoDB Atlas!');
 
 
-        
 
-  
+
+
 
         return mongoClient
     } catch (error) {
